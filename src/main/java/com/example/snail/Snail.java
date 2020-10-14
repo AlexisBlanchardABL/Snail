33e56119fcca504d008f4aca97e27a2307f45331
BLANCHARD Alexis <alexis-externe.blanchard@dalkia.fr>
2020-10-14 15:56:32 +0200

    Make snailTest1,2,3,4,5 pass

--- src/main/java/com/example/snail/Snail.java
+++ src/main/java/com/example/snail/Snail.java
@@ -1,7 +1,52 @@
 package com.example.snail;
 
 public class Snail {
-    public int[][] snail(int n) {
-        return new int[0][];
+    private final char width;
+    private final int[][] snail;
+    private int posX = 0;
+    private int posY = 0;
+    private int count = 1;
+
+    Snail(char width) {
+        snail = new int[width][width];
+        this.width = width;
     }
+
+    public int[][] compute() {
+        fillTop();
+        fillRight();
+        fillBottom();
+        fillLeft();
+        if (count <= width * width) {
+            posX++;
+            posY++;
+            return compute();
+        }
+        return snail;
+    }
+
+    private void fillRight() {
+        for (int i = 1 + posY; i < width - posY; i++) {
+            snail[i][width - 1 - posX] = count++;
+        }
+    }
+
+    private void fillLeft() {
+        for (int i = width - 2 - posY; i > posY; i--) {
+            snail[i][posX] = count++;
+        }
+    }
+
+    private void fillTop() {
+        for (int i = posX; i < width - posX; i++) {
+            snail[posY][i] = count++;
+        }
+    }
+
+    private void fillBottom() {
+        for (int i = width - 2 - posX; i >= posX; i--) {
+            snail[width - 1 - posY][i] = count++;
+        }
+    }
+
 }
--- src/test/java/com/example/snail/SnailTest.java
+++ src/test/java/com/example/snail/SnailTest.java
@@ -1,6 +1,5 @@
 package com.example.snail;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
@@ -8,41 +7,122 @@ import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 public class SnailTest {
     Snail snail;
 
-    @BeforeEach
-    public void setup() {
-        snail = new Snail();
-    }
-
     @Test
-    public void testSnail1() {
+    public void testSnail_1() {
         //given
-        int n = 1;
+        char width = 1;
+        snail = new Snail(width);
         int[][] expected = {{1}};
         //when
-        int[][] actual = this.snail.snail(n);
+        int[][] actual = this.snail.compute();
         //then
         assertArrayEquals(expected, actual);
     }
 
     @Test
-    public void testSnail2() {
+    public void testSnail_2() {
         //given
-        int n = 2;
-        int[][] expected = {{1, 2}, {4, 3}};
+        char width = 2;
+        snail = new Snail(width);
+        int[][] expected = {
+                {1, 2},
+                {4, 3}
+        };
         //when
-        int[][] actual = this.snail.snail(n);
+        int[][] actual = snail.compute();
         //then
         assertArrayEquals(expected, actual);
     }
 
     @Test
-    public void testSnail3() {
+    public void testSnail_3() {
         //given
-        int n = 3;
+        char width = 3;
+        snail = new Snail(width);
+        /**
+         * 1 2 3
+         * 8 9 4
+         * 7 6 5
+         */
         int[][] expected = {{1, 2, 3}, {8, 9, 4}, {7, 6, 5}};
         //when
-        int[][] actual = this.snail.snail(n);
+        int[][] actual = snail.compute();
         //then
         assertArrayEquals(expected, actual);
     }
+
+    @Test
+    public void testSnail_4() {
+        //given
+        char width = 4;
+        snail = new Snail(width);
+        /**
+         * 1  2  3  4
+         * 12 13 14 5
+         * 11 16 15 6
+         * 10  9  8 7
+         */
+        int[][] expected = {
+                {1, 2, 3, 4},
+                {12, 13, 14, 5},
+                {11, 16, 15, 6},
+                {10, 9, 8, 7}
+        };
+        //when
+        int[][] actual = snail.compute();
+        //then
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void testSnail_5() {
+        //given
+        char width = 5;
+        snail = new Snail(width);
+        /**
+         * 1  2  3  4  5
+         * 16 17 18 19 6
+         * 15 24 25 20 7
+         * 14 23 22 21 8
+         * 13 12 11 10 9
+         */
+        int[][] expected = {
+                {1, 2, 3, 4, 5},
+                {16, 17, 18, 19, 6},
+                {15, 24, 25, 20, 7},
+                {14, 23, 22, 21, 8},
+                {13, 12, 11, 10, 9}
+        };
+        //when
+        int[][] actual = snail.compute();
+        //then
+
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void testSnail_6() {
+        char width = 6;
+        snail = new Snail(width);
+        int[][] expected = {
+                {1, 2, 3, 4, 5, 6},
+                {20, 21, 22, 23, 24, 7},
+                {19, 32, 33, 34, 25, 8},
+                {18, 31, 36, 35, 26, 9},
+                {17, 30, 29, 28, 27, 10},
+                {16, 15, 14, 13, 12, 11}
+        };
+        int[][] actual = snail.compute();
+        //then
+
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void testSnail_100() {
+        char width = 100;
+        snail = new Snail(width);
+        snail.compute();
+    }
+
 }
